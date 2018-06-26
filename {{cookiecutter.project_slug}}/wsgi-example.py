de726dc5540377cc3c12d983cd53c1dc6666db09
Jonatas Leon <jonatas.leon@gmail.com>
2018-06-26 10:44:15 -0300

    Update wsgi-example.py

--- {{cookiecutter.project_slug}}/wsgi-example.py
+++ {{cookiecutter.project_slug}}/wsgi-example.py
@@ -9,7 +9,7 @@ cfg_file = '/path/to/config.cfg'
 if path not in sys.path:
     sys.path.append(path)
 
-from core import create_app
+from {{cookiecutter.project_slug}} import create_app
 
 # It's necessary have an object application
 application = create_app(cfg_file=cfg_file)
